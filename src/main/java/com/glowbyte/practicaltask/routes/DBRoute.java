e06a495391cb2fe3acc86a0395a172a3276aafa1
HvarZ <hvarzahar@gmail.com>
2023-02-28 15:02:13 +0300

    try first route with groovy

--- src/main/java/com/glowbyte/practicaltask/routes/DBRoute.java
+++ src/main/java/com/glowbyte/practicaltask/routes/DBRoute.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
-import com.glowbyte.practicaltask.processor.XMLMarshaller;
-
 
 @Component
 public class DBRoute extends RouteBuilder {
@@ -17,24 +15,11 @@ public class DBRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("timer:timer?period=60_000")
-        .to("sql:select  a.*, " +
-                "        c.client_id, c.firstname, c.surname, c.lastname, c.birthdate, c.birthplace, " +
-                "        addr.address_id, addr.country, addr.city, addr.street, addr.house, addr.building, addr.flat, " +
-                "        i.income_id, i.client_id, i.month, i.amount " +
-                "from Application a " +
-                "    left outer join " +
-                "        Income i " +
-                "    on (a.application_id = i.application_id) " +
-                "    left outer join " +
-                "        Client c " +
-                "            left outer join " +
-                "                Address addr " +
-                "            on (c.client_id = addr.client_id) " +
-                "    on (a.application_id = c.application_id)" +
-                "order by application_id;")
-        .routeId("test route")
-        .log("<<<<<<<<<<<<< ${body}")
-        .process(new XMLMarshaller())
-        .to("activemq:marshall-test");
+                .routeId("db_route")
+                .setBody().groovy("resource:classpath:com.glowbyte.practicaltask.groovy_db.db_to_xml")
+                .marshal().jaxb()
+                .log(">>>>>>>>>>>> ${body}")
+                .to("log:log");
+
     }
 }
