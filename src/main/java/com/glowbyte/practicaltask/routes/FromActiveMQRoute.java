93b645a56385810de22d0c1d123945026fa76248
HvarZ <hvarzahar@gmail.com>
2023-03-07 13:46:22 +0300

    first init for first route

---
+++ src/main/java/com/glowbyte/practicaltask/routes/FromActiveMQRoute.java
@@ -0,0 +1,34 @@
+package com.glowbyte.practicaltask.routes;
+
+import com.glowbyte.practicaltask.entity.Application;
+import com.glowbyte.practicaltask.entity.Income;
+import com.glowbyte.practicaltask.repository.ApplicationRepo;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.converter.jaxb.JaxbDataFormat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.xml.bind.JAXBContext;
+
+@Component
+public class FromActiveMQRoute extends RouteBuilder {
+    @Autowired
+    ApplicationRepo applicationRepo;
+    @Override
+    public void configure() throws Exception {
+        JAXBContext context = JAXBContext.newInstance(Application.class);
+        JaxbDataFormat jaxbDataFormat = new JaxbDataFormat();
+        jaxbDataFormat.setContext(context);
+
+        from("activemq:test_1")
+                .routeId("First Route")
+                .unmarshal(jaxbDataFormat)
+                .process(exchange -> {
+                    Application application = (Application) exchange.getIn().getBody();
+                    //applicationRepo.save(application);
+                    exchange.getIn().setBody(application);
+                })
+                .marshal().jaxb()
+                .log("\n${body}");
+    }
+}
