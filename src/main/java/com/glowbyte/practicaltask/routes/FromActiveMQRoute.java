71702294546baaa081222cea577713fd69f0d4fd
HvarZ <hvarzahar@gmail.com>
2023-03-09 19:07:24 +0300

    finish route1

--- src/main/java/com/glowbyte/practicaltask/routes/FromActiveMQRoute.java
+++ src/main/java/com/glowbyte/practicaltask/routes/FromActiveMQRoute.java
@@ -1,9 +1,13 @@
 package com.glowbyte.practicaltask.routes;
 
 import com.glowbyte.practicaltask.entity.Application;
+import com.glowbyte.practicaltask.entity.json.InKafka;
 import com.glowbyte.practicaltask.processors.DBFiller;
+import com.glowbyte.practicaltask.processors.XmlToJsonPOJO;
 import com.glowbyte.practicaltask.repository.ApplicationRepo;
+import lombok.Cleanup;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.apache.camel.converter.jaxb.JaxbDataFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -21,11 +25,15 @@ public class FromActiveMQRoute extends RouteBuilder {
         JaxbDataFormat jaxbDataFormat = new JaxbDataFormat();
         jaxbDataFormat.setContext(context);
 
+        @Cleanup
+        JacksonDataFormat jsonDataFormat = new JacksonDataFormat(InKafka.class);
+
         from("activemq:test_1")
                 .routeId("First Route")
                 .unmarshal(jaxbDataFormat)
                 .process(new DBFiller(applicationRepo))
-                .marshal().jaxb()
-                .log("\n${body}");
+                .process(new XmlToJsonPOJO())
+                .marshal(jsonDataFormat)
+                .to("kafka:route_1");
     }
 }
