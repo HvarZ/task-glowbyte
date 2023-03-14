0b96e9c3fe5db6ffc0005f8b57e5cd8408af182d
HvarZ <hvarzahar@gmail.com>
2023-03-14 20:11:43 +0300

    add new processor for third route

---
+++ src/main/java/com/glowbyte/practicaltask/processors/AnswerJsonToAnswerXML.java
@@ -0,0 +1,26 @@
+package com.glowbyte.practicaltask.processors;
+
+import com.glowbyte.practicaltask.entity.XMLAnswer.Application;
+import com.glowbyte.practicaltask.entity.XMLAnswer.Decision;
+import com.glowbyte.practicaltask.entity.XMLAnswer.XMLAnswer;
+import com.glowbyte.practicaltask.entity.json_in.InClient;
+import com.glowbyte.practicaltask.entity.json_in.InKafka;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+public class AnswerJsonToAnswerXML implements Processor {
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        InKafka answer = (InKafka) exchange.getIn().getBody();
+        Decision decision = Decision.APPROVED;
+
+        for (InClient client : answer.getClients()) {
+            if (!client.isSolvent()) {
+                decision = Decision.DECLINE;
+                break;
+            }
+        }
+
+        exchange.getIn().setBody(XMLAnswer.builder().application(new Application(answer.getId())).decision(decision));
+    }
+}
