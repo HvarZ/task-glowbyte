8ae1ed9337731c178293a1c2d6d534f0bca87c8c
HvarZ <hvarzahar@gmail.com>
2023-03-15 12:33:00 +0300

    add marshal and unmarshal

--- src/main/java/com/glowbyte/practicaltask/routes/FromKafkaToDBAndActiveMQ.java
+++ src/main/java/com/glowbyte/practicaltask/routes/FromKafkaToDBAndActiveMQ.java
@@ -1,13 +1,19 @@
 package com.glowbyte.practicaltask.routes;
 
+import com.glowbyte.practicaltask.entity.json_in.InKafka;
 import com.glowbyte.practicaltask.processors.AnswerJsonToAnswerXML;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jackson.JacksonDataFormat;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FromKafkaToDBAndActiveMQ extends RouteBuilder {
     @Override
-    public void configure() throws Exception {
+    public void configure() {
         from("kafka:topic_2?groupId=from_topic_2")
+                .unmarshal(new JacksonDataFormat(InKafka.class))
                 .process(new AnswerJsonToAnswerXML())
+                .marshal().jaxb()
                 .to("activemq:answer");
     }
 }
